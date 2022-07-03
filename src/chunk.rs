bd4dde4443834e930daf7d0296e98d6b62afb37a
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2022-07-03 19:16:32 +0100

    refactor: use macro to create enum

--- src/chunk.rs
+++ src/chunk.rs
@@ -2,14 +2,41 @@
 
 use crate::value::{Value, ValueArray};
 
-/// A one-byte operation code for Lox.
+/// Derives a TryFrom<u8> implementation for the enum.
 ///
-/// (See Crafting Interpreters, p. 244)
-#[repr(u8)]
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub enum OpCode {
-    Return,
-    Constant,
+/// Macro adapted from: https://stackoverflow.com/a/57578431/6626414
+macro_rules! with_try_from_u8 {
+    ($(#[$meta:meta])* $vis:vis enum $name:ident {
+        $($(#[$vmeta:meta])* $vname:ident $(= $val:expr)?,)*
+    }) => {
+        $(#[$meta])*
+        $vis enum $name {
+            $($(#[$vmeta])* $vname $(= $val)?,)*
+        }
+
+        impl std::convert::TryFrom<u8> for $name {
+            type Error = ();
+
+            fn try_from(v: u8) -> Result<Self, Self::Error> {
+                match v {
+                    $(x if x == $name::$vname as u8 => Ok($name::$vname),)*
+                    _ => Err(()),
+                }
+            }
+        }
+    }
+}
+
+with_try_from_u8! {
+    /// A one-byte operation code for Lox.
+    ///
+    /// (See Crafting Interpreters, p. 244)
+    #[repr(u8)]
+    #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+    pub enum OpCode {
+        Return,
+        Constant,
+    }
 }
 
 /// A chunk of code, with metadata.
@@ -133,19 +160,6 @@ impl<'a> WrittenOpcode<'a> {
     #[inline]
     pub fn with_operand(self, index: u8) {
         self.provenance.write(index, self.line);
-    }
-}
-
-impl TryFrom<u8> for OpCode {
-    // TODO: temporary
-    type Error = ();
-
-    fn try_from(byte: u8) -> Result<Self, Self::Error> {
-        match byte {
-            x if x == OpCode::Return as u8 => Ok(OpCode::Return),
-            x if x == OpCode::Constant as u8 => Ok(OpCode::Constant),
-            _ => Err(()),
-        }
     }
 }
 
